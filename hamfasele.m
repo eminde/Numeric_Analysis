4e593063f936cb1edee379fce8bc558384ce122d
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-05-21 21:36:23 +0430

    add 50 and 100 points.

--- hamfasele.m
+++ hamfasele.m
@@ -12,27 +12,68 @@ ronge_func = @(x)(1./(1+x.^2));
 %Example NewtonIntercep for ronge Funcion  , degree = 10 , chobeishof
 %points
 
-n=10;
-x =linspace(-5,5,11);
+%daraje 10
+x11 =linspace(-5,5,11);
+%daraje 50
+x51 =linspace(-5,5,51);
+%daraje 100
+x101 =linspace(-5,5,101);
 
-f = ronge_func(x);
-g = abs_func(x);
-h = sin_func(x);
-s = -5:0.01:5;
 
-[p1,a1,D1] = NewtonInterp(x,f,s);
-[p2,a2,D2] = NewtonInterp(x,g,s);
-[p3,a3,D3] = NewtonInterp(x,h,s);
+f11 = ronge_func(x11);
+f51 = ronge_func(x51);
+f101 = ronge_func(x101);
+
+g11 = abs_func(x11);
+g51 = abs_func(x51);
+g101 = abs_func(x101);
+
+h11 = sin_func(x11);
+h51 = sin_func(x51);
+h101 = sin_func(x101);
+
+s = -5:0.005:5;
+
+[p11f,a11f,D11f] = NewtonInterp(x11,f11,s);
+[p51f,a51f,D51f] = NewtonInterp(x51,f51,s);
+[p101f,a101f,D101f] = NewtonInterp(x101,f101,s);
+
+[p11g,a11g,D11g] = NewtonInterp(x11,g11,s);
+[p51g,a51g,D51g] = NewtonInterp(x51,g51,s);
+[p101g,a101g,D101g] = NewtonInterp(x101,g101,s);
+
+[p11h,a11h,D11h] = NewtonInterp(x11,h11,s);
+[p51h,a51h,D51h] = NewtonInterp(x51,h51,s);
+[p101h,a101h,D101h] = NewtonInterp(x101,h101,s);
+
 
 figure
-subplot(3,1,1);
-plot (x,g,'ok',s,abs_func(s),'-b',s,p2,'--k');
+subplot(3,3,1);
+plot (x11,g11,'ok',s,abs_func(s),'-b',s,p11g,'--k');
+title('Abs - hamfasele')
+subplot(3,3,2);
+plot (x51,g51,'ok',s,abs_func(s),'-b',s,p51g,'--k');
+title('Abs - hamfasele')
+subplot(3,3,3);
+plot (x101,g101,'ok',s,abs_func(s),'-b',s,p101g,'--k');
 title('Abs - hamfasele')
 
-subplot(3,1,2);
-plot (x,f,'ok',s,ronge_func(s),'-b',s,p1,'--k');
+subplot(3,3,4);
+plot (x11,f11,'ok',s,ronge_func(s),'-b',s,p11f,'--k');
+title('Ronge -  hamfasele')
+subplot(3,3,5);
+plot (x51,f51,'ok',s,ronge_func(s),'-b',s,p51f,'--k');
+title('Ronge -  hamfasele')
+subplot(3,3,6);
+plot (x101,f101,'ok',s,ronge_func(s),'-b',s,p101f,'--k');
 title('Ronge -  hamfasele')
 
-subplot(3,1,3);
-plot (x,h,'ok',s,sin_func(s),'-b',s,p3,'--k');
+subplot(3,3,7);
+plot (x11,h11,'ok',s,sin_func(s),'-b',s,p11h,'--k');
+title('Sin - hamfasele')
+subplot(3,3,8);
+plot (x51,h51,'ok',s,sin_func(s),'-b',s,p51h,'--k');
+title('Sin - hamfasele')
+subplot(3,3,9);
+plot (x101,h101,'ok',s,sin_func(s),'-b',s,p101h,'--k');
 title('Sin - hamfasele')
