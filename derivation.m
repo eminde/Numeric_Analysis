010a8cc8d88c25ddd22e4973b9aa6668d58c0126
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-05-23 10:07:15 +0430

    add derivation.

---
+++ derivation.m
@@ -0,0 +1,12 @@
+f = @(x) exp(x);
+h(1)=0.1;
+K=10;
+
+for k = 1:K
+    F = (f(h(k))-f(0))/h(k);
+    err(k)=abs(F-f(0));
+    h(k+1)=h(k)/2;
+end
+loglog(h(1:end-1),err)
+xlable('h');
+ylable('Error');
