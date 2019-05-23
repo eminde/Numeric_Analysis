e93e1bb8a8b0779d61c9872187014705538f395f
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-05-23 19:49:41 +0430

    update der add pishrp and pasro flag

--- derivation.m
+++ derivation.m
@@ -1,11 +1,15 @@
 f = @(x) exp(x);
+x0=0;
+
+cf=1;
 h(1)=0.1;
 % K=40;
-K=10;
+K=40;
 
 for k = 1:K
-    F = (f(h(k))-f(0))/h(k);
-    err(k)=abs(F-f(0));
+    x1 = x0 +cf*h(k)
+    F = abs(f(x0)-f(x1))/h(k);
+    err(k)=abs(F-f(x0));
     h(k+1)=h(k)/2;
 end
 loglog(h(1:end-1),err)
